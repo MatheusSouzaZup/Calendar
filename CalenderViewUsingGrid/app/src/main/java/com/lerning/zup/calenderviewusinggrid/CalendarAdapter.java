56d20733d0ad5740496c8b6e33c1e2f4ecf5cc96
Matheus Souza <matheus.souza@zup.com.br>
2017-12-04 17:50:52 -0200

    First Part Done

--- CalenderViewUsingGrid/app/src/main/java/com/lerning/zup/calenderviewusinggrid/CalendarAdapter.java
+++ CalenderViewUsingGrid/app/src/main/java/com/lerning/zup/calenderviewusinggrid/CalendarAdapter.java
@@ -1,6 +1,6 @@
 package com.lerning.zup.calenderviewusinggrid;
 
-import android.content.Context;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,6 +9,8 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 /**
@@ -16,71 +18,99 @@ import java.util.List;
  */
 
 public class CalendarAdapter extends RecyclerView.Adapter<CalendarAdapter.CalendarViewHolder> {
-    private LayoutInflater mLayoutInflater;
-    Context mContext;
-    List<Day> mList;
-    private OnCalenderListener onCalenderListener;
 
-    public CalendarAdapter(List<Day> list, OnCalenderListener calenderListener) {
-        mList = list;
-        onCalenderListener = calenderListener;
-    }
-    public CalendarAdapter(List<Day> list) {
-        mList = list;
+    List<Day> mList;
+
+    public CalendarAdapter(Calendar calendar) {
+        mList = getMonthConfigurated(calendar);
     }
 
     @Override
     public CalendarViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        mLayoutInflater = (LayoutInflater) parent.getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        View view = mLayoutInflater.inflate(R.layout.item_day_calendar, parent, false);
-        mContext = parent.getContext();
-        CalendarViewHolder calendarViewHolder = new CalendarViewHolder(view);
-        return calendarViewHolder;
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_day_calendar, parent, false);
+        return new CalendarViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(final CalendarViewHolder holder, final int position) {
-        if (mList.get(position) != null) {
-            holder.onBind(mList.get(position));
-        } else {
+        Day day = mList.get(position);
+        if (day == null){
             return;
         }
-
+        holder.onBind(day);
     }
 
     @Override
     public int getItemCount() {
-        if (mList == null)
-            return 0;
-        return mList.size();
+        return mList == null? 0 : mList.size();
+    }
+
+    private MyCalendar setUpValidyDate(Calendar calendar) {
+        MyCalendar myCalendar = new MyCalendar();
+        myCalendar.setFirstDay(calendar.get(Calendar.DAY_OF_WEEK));
+        myCalendar.setMonthSize(calendar.getActualMaximum(Calendar.DAY_OF_MONTH));
+        myCalendar.setUsedDays(getUsedDays());
+        return myCalendar;
+    }
+
+    private List<Day> getMonthConfigurated(Calendar calendar) {
+        MyCalendar myCalendar = setUpValidyDate(calendar);
+        List<Day> mList = new ArrayList<>();
+        fillWithEmptyDay(myCalendar, mList);
+        createUsefulDays(myCalendar, mList);
+        return mList;
+    }
+
+    private void createUsefulDays(MyCalendar myCalendar, List<Day> mList) {
+        for(int i = 1; i <= myCalendar.getMonthSize(); i++) {
+            boolean isUsedDay = myCalendar.getUsedDays().contains(i);
+            mList.add(new Day(i + "", isUsedDay));
+        }
+    }
+
+    private void fillWithEmptyDay(MyCalendar myCalendar, List<Day> mList) {
+        for (int i = 0; i < myCalendar.getFirstDay() -1; i++){
+            mList.add(null);
+        }
+    }
+
+    private List<Integer> getUsedDays() {
+        List<Integer> mList = new ArrayList<>();
+        mList.add(1);
+        mList.add(5);
+        mList.add(9);
+        mList.add(13);
+        mList.add(21);
+        return mList;
     }
 
     public class CalendarViewHolder extends RecyclerView.ViewHolder {
-        TextView tvDay;
-        ImageView ivDot;
+        TextView mTvDay;
+        ImageView mIvDot;
 
         public CalendarViewHolder(View itemView) {
             super(itemView);
-            tvDay = itemView.findViewById(R.id.calendar_day);
-            ivDot = itemView.findViewById(R.id.calendar_dot);
+            mTvDay = itemView.findViewById(R.id.calendar_day);
+            mIvDot = itemView.findViewById(R.id.calendar_dot);
         }
 
         public void onBind(final Day day) {
+            mTvDay.setText(day.getDay());
             if (day.isUsedDay()) {
-                ivDot.setVisibility(View.VISIBLE);
-                tvDay.setTextColor(mContext.getResources().getColor(R.color.colorBlack));
-                tvDay.setText(day.getDay());
-                itemView.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        Toast.makeText(mContext, day.getDay(), Toast.LENGTH_SHORT).show();
-                        //onCalenderListener.onClickItemDay();
-                    }
-                });
-            } else {
-                tvDay.setTextColor(mContext.getResources().getColor(R.color.colorGrayLight));
-                tvDay.setText(day.getDay());
+                enable(day);
             }
+        }
+
+        private void enable(final Day day) {
+            mIvDot.setVisibility(View.VISIBLE);
+            mTvDay.setTextColor(ContextCompat.getColor(itemView.getContext(), R.color.colorBlack));
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(itemView.getContext(), day.getDay(), Toast.LENGTH_SHORT).show();
+                    //onCalenderListener.onClickItemDay();
+                }
+            });
         }
     }
 
--- CalenderViewUsingGrid/app/src/main/java/com/lerning/zup/calenderviewusinggrid/MainActivity.java
+++ CalenderViewUsingGrid/app/src/main/java/com/lerning/zup/calenderviewusinggrid/MainActivity.java
@@ -17,50 +17,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mRecyclerView = (RecyclerView) findViewById(R.id.calendar);
-        MyCalendar myCalendar = setDate(new GregorianCalendar(2017,12-1,1));
-        mAdapter = new CalendarAdapter(getMonthConfigurated(myCalendar));
+        mRecyclerView = findViewById(R.id.calendar);
+
+        mAdapter = new CalendarAdapter(new GregorianCalendar(2018,1-1,1));
         GridLayoutManager gridLayoutManager = new GridLayoutManager(getApplicationContext(),7);
         mRecyclerView.setLayoutManager(gridLayoutManager);
         mRecyclerView.setAdapter(mAdapter);
+    }
 
-    }
-    private List<Integer> getUsedDays() {
-        List<Integer> mList = new ArrayList<>();
-        mList.add(1);
-        mList.add(5);
-        mList.add(9);
-        mList.add(13);
-        mList.add(21);
-        return mList;
-    }
-    public MyCalendar setDate(Calendar calendar) {
-        MyCalendar myCalendar = new MyCalendar();
-        myCalendar.setFirstDay(calendar.get(Calendar.DAY_OF_WEEK));
-        myCalendar.setMonthSize(calendar.getActualMaximum(Calendar.DAY_OF_MONTH));
-        myCalendar.setUsedDays(getUsedDays());
-        return myCalendar;
-    }
-    public List<Day> getMonthConfigurated(MyCalendar myCalendar) {
-        List<Day> mList = new ArrayList<>();
-        int listSize;
-        Integer mDays = 1;
 
-        listSize = myCalendar.getFirstDay() + myCalendar.getMonthSize();
-
-        for(Integer i = 1; i <listSize; i++) {
-            if(i < myCalendar.getFirstDay()) {
-                mList.add(null);
-            }
-            else {
-                if(myCalendar.getUsedDays().contains(mDays))
-                    mList.add(new Day(mDays.toString(),true));
-                else {
-                    mList.add(new Day(mDays.toString(),false));
-                }
-                mDays++;
-            }
-        }
-        return mList;
-    }
 }
